17087b7fada70d9e0d7bc65b0046308c9ba05a6f
Neo-PC <galinabunu26@gmail.com>
2026-05-30 13:25:57 +0200

    Initial commit

---
+++ src/test/java/pixel/academy/crud/app/CrudAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pixel.academy.crud.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
